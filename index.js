05f9681c73de437f6c8be1054165eb61b381089e
Mees van Dijk <mees@mees.io>
2023-01-06 12:10:35 +0100

    Convert to ESM

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const debug = require('debug')('koa-easy-ws')
-const http = require('http')
-const process = require('process')
-const WebSocket = require('ws')
+import makeDebug from 'debug'
+import http from 'node:http'
+import process from 'node:process'
+import WebSocket from 'ws'
+
+const debug = makeDebug('koa-easy-ws')
 
 const serversPatched = new WeakSet()
 
@@ -9,12 +11,19 @@ function createWebsocketMiddleware (propertyName = 'ws', options) {
   if (!options) options = {}
   if (options instanceof http.Server) options = { server: options }
 
-  if (parseInt(process.versions.node) < 10 && !options.noServerWorkaround) { // node 9 or earlier needs a workaround for upgrade requests
+  if (parseInt(process.versions.node) < 10 && !options.noServerWorkaround) {
+    // node 9 or earlier needs a workaround for upgrade requests
     if (!options.server) {
-      throw new TypeError(`
+      throw new TypeError(
+        `
         If you target Node 9 or earlier you must provide the HTTP server either as an option or as the second parameter.
         See the readme for more instructions: https://github.com/b3nsn0w/koa-easy-ws#special-usage-for-node-9-or-earlier
-      `.trim().split('\n').map(s => s.trim()).join('\n'))
+      `
+          .trim()
+          .split('\n')
+          .map(s => s.trim())
+          .join('\n')
+      )
     } else {
       if (!serversPatched.has(options.server)) {
         serversPatched.add(options.server)
@@ -33,13 +42,14 @@ function createWebsocketMiddleware (propertyName = 'ws', options) {
 
     if (~upgradeHeader.indexOf('websocket')) {
       debug(`websocket middleware in use on route ${ctx.path}`)
-      ctx[propertyName] = () => new Promise((resolve) => {
-        wss.handleUpgrade(ctx.req, ctx.request.socket, Buffer.alloc(0), (ws) => {
-          resolve(ws)
-          wss.emit('connection', ws, ctx.req)
+      ctx[propertyName] = () =>
+        new Promise(resolve => {
+          wss.handleUpgrade(ctx.req, ctx.request.socket, Buffer.alloc(0), ws => {
+            resolve(ws)
+            wss.emit('connection', ws, ctx.req)
+          })
+          ctx.respond = false
         })
-        ctx.respond = false
-      })
       if (options.exposeServerOn) ctx[options.exposeServerOn] = wss
     }
 
@@ -50,4 +60,4 @@ function createWebsocketMiddleware (propertyName = 'ws', options) {
   return websocketMiddleware
 }
 
-module.exports = createWebsocketMiddleware
+export default createWebsocketMiddleware
